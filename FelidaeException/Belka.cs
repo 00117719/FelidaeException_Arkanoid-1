e0f51d3ac34b0802ad04ccdac43510314240af41
GatoHery <54414691+GatoHery@users.noreply.github.com>
2020-05-14 14:09:31 -0600

    add: menu

---
+++ FelidaeException/Belka.cs
@@ -0,0 +1,36 @@
+﻿namespace FelidaeException
+{
+    public class Belka
+    {
+        public void move(OknoGry okno, Paddle belka)
+        {
+            if (visible)
+            {
+                x += speedX;
+                y += speedY;
+
+                if (x < 0)
+                {
+                    x = 0;
+                    speedX *= -1;
+                }
+                else if (x + width > okno.Width)
+                {
+                    x = okno.Width - width;
+                    speedX *= -1;
+                }
+
+                if (((x > belka.getX() && x < belka.getX() + belka.getWidth()) || (x + width > belka.getX() && x + width < belka.getX() + belka.getWidth())) &&
+                    (y + height > belka.getY() && y + height < belka.getY() + belka.getHeight()))//warunek zderzenia z belka
+                {
+                    applyBonus(bonus, belka, okno.pilka, okno.lista, okno);
+                    dezaktywuj();
+                }
+                else if (y + height > okno.Height)
+                {
+                    dezaktywuj();
+                }
+            }
+        }
+    }
+}
---
+++ FelidaeException/Form2.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace FelidaeException
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
